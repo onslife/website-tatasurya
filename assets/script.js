905b874485f6835fff320341c27e5b65137cc7b9
onslife <95129178+onslife@users.noreply.github.com>
2022-05-07 19:32:12 +0700

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,39 @@
+let menu = document.querySelector(".menu")
+let nav = document.querySelector("#nav")
+
+menu.addEventListener("click", function () {
+    nav.classList.toggle("sidebar")
+    menu.classList.toggle("change")
+})
+
+document.addEventListener("scroll", function () {
+    menu.classList.remove("change")
+    nav.classList.remove("sidebar")
+})
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
